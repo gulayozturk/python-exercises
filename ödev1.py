9e6b54d4b9a64f2b7a9ab289f223df9e4eb92afa
gulayozturk <118389346+gulayozturk@users.noreply.github.com>
2022-11-17 11:29:39 +0300

    Add files via upload

---
+++ ödev1.py
@@ -0,0 +1,20 @@
+
+
+baslik = "Haberiniz Olsun"     #String
+vade = 15
+faizOrani1 = 1.47    #float
+faizOrani2 = 1.44
+
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani1))
+print(type(faizOrani2))
+
+mesaj = "Hoşgeldin"
+musteriAdi = "Gülay"
+musteriSoyadi = "Öztürk"
+sonucMesaj = mesaj + " " + musteriAdi + " " + musteriSoyadi + "!"
+
+print(sonucMesaj)
+
