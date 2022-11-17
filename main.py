9981143b5dae9afc97489ac07c5adc1d7802620b
gulayozturk <118389346+gulayozturk@users.noreply.github.com>
2022-11-17 09:46:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+print("Merhaba Etiya")
+
+# string = metin ifadesi
+text = "17"
+print(text)
+
+number = 12
+print(number)
+
+# double, float,decimal =kesirli sayi
+
+dnumber = 4.8
+print(dnumber)
+
+# matematiksel operatorler
+
+print(number +3)
+print(number -3)
+print(number /3)
+print(number *3)
+print(number %3)
+
+#!boolean,bool = true and false 
+
+print(1 == 1)
+print(1 == 2)
+print(1 != 2)
+
+print(6 >= 2)
+print(6 <= 1)
+
+name = "Gulay"
+age = 29
+company = "Etiya"
+
+# Gulay 29 yasinda Etiya da calisiyor
+# print(name + " " + age + " " + company + " " da calisiyor)
+
+print(f"{name}  {age} yasinda  {company} da calisiyor")
+
